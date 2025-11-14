3fa8f5a682341b83f51e16850627fb71a0455ca0
aasthananda <77813827+aasthananda@users.noreply.github.com>
2025-11-14 07:41:13 -0800

    Update GameGUI.java

--- GameGUI.java
+++ GameGUI.java
@@ -1,331 +1,207 @@
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.Image;
-import java.awt.Point;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import java.io.File;
-import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.geom.*;
 import java.util.Random;
-//initialization stuff
-public class GameGUI extends JComponent
+import java.util.ArrayList;
+
+public class GameGUI extends JPanel implements ActionListener
 {
-    static final long serialVersionUID = 141L;
+    JFrame frame;
+    Timer timer;
+    int delay = 60;
 
-    private static final int WIDTH = 510;
-    private static final int HEIGHT = 360;
-    private static final int SPACE_SIZE = 60;
-    private static final int GRID_W = 8;
-    private static final int GRID_H = 5;
-    private static final int START_LOC_X = 15;
-    private static final int START_LOC_Y = 15;
+    int w, h, s;
+    int x, y;
+    int steps;
+    int goal;
+    int scoreVal = 5;
+    int wallVal = -1;
+    int trapVal = -20;
 
-    int x = START_LOC_X;
-    int y = START_LOC_Y;
+    ArrayList<Rectangle> walls;
+    ArrayList<Rectangle> traps;
+    ArrayList<Ellipse2D.Double> prizes;
 
-    private Image bgImage;
-    private Image player;
-    private Image prizeImage;
-    private Image rockImage;
-    private Image dukeImage;
+    Image player;
+    Image rock;
+    Image flag;
 
-    private Point playerLoc;
-    private int playerSteps;
-
-    private int totalWalls;
-    private Rectangle[] walls;
-    private int totalPrizes;
-    private Rectangle[] prizes;
-    private int totalTraps;
-    private Rectangle[] traps;
-
-    private int prizeVal = 10;
-    private int trapVal = 5;
-    private int endVal = 10;
-    private int offGridVal = 5;
-    private int hitWallVal = 5;
-
-    private JFrame frame;
-
-    private int dukeX;
-    private int dukeY;
-    private int dukeWidth = 60;
-    private int dukeHeight = 60;
+    boolean repaintNeeded;
 
     public GameGUI()
     {
-        try { bgImage = ImageIO.read(new File("grid.png")); } 
-        catch (Exception e) { System.err.println("Could not open file grid.png"); }
+        w = 10;
+        h = 10;
+        s = 60;
+        x = s/2;
+        y = s/2;
+        steps = 0;
+        goal = w*s + s/2;
 
-        try { prizeImage = ImageIO.read(new File("coin.png")); } 
-        catch (Exception e) { System.err.println("Could not open file coin.png"); }
+        walls = new ArrayList<>();
+        traps = new ArrayList<>();
+        prizes = new ArrayList<>();
 
-        try { player = ImageIO.read(new File("player.png")); } 
-        catch (Exception e) { System.err.println("Could not open file player.png"); }
+        player = new ImageIcon("small-duke.png").getImage();
+        rock = new ImageIcon("rock.png").getImage();
+        flag = new ImageIcon("flag.png").getImage();
 
-        try { rockImage = ImageIO.read(new File("rock.png")); } 
-        catch (Exception e) { System.err.println("Could not open file rock.png"); }
+        timer = new Timer(delay, this);
+    }
 
-        try { dukeImage = ImageIO.read(new File("dukeLarge.png")); } 
-        catch (Exception e) { System.err.println("Could not open file dukeLarge.png"); }
+    public void createBoard()
+    {
+        walls.clear();
+        traps.clear();
+        prizes.clear();
 
-        playerLoc = new Point(x, y);
-//sets up file 
-        frame = new JFrame();
-        frame.setTitle("EscapeRoom");
-        frame.setSize(WIDTH, HEIGHT);
+        Random rand = new Random();
+
+        for (int i=1; i<=5; i++)
+        {
+            int wx = rand.nextInt(w)*s;
+            int wy = rand.nextInt(h)*s;
+            Rectangle r;
+            if (rand.nextInt(2)==0)
+                r = new Rectangle(wx+s-5, wy, 8, s);
+            else
+                r = new Rectangle(wx, wy+s-5, s, 8);
+            walls.add(r);
+        }
+
+        for (int i=0; i<5; i++)
+        {
+            int tx = rand.nextInt(w)*s + 10;
+            int ty = rand.nextInt(h)*s + 10;
+            traps.add(new Rectangle(tx, ty, 40, 40));
+        }
+
+        for (int i=0; i<3; i++)
+        {
+            int px = rand.nextInt(w)*s + 20;
+            int py = rand.nextInt(h)*s + 20;
+            prizes.add(new Ellipse2D.Double(px, py, 20, 20));
+        }
+
+        x = s/2;
+        y = s/2;
+        steps = 0;
+
+        frame = new JFrame("Escape Room");
+        frame.setSize(w*s+100, h*s+100);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(this);
         frame.setVisible(true);
-        frame.setResizable(false);
-//sets the numerical limits that are called later on to prevent a map filled with one thing or the other
-        totalWalls = 20;
-        totalPrizes = 3;
-        totalTraps = 5;
-//generates duke placements 
-        Random rand = new Random();
-        dukeX = rand.nextInt(GRID_W) * SPACE_SIZE + 15;
-        dukeY = rand.nextInt(GRID_H) * SPACE_SIZE + 15;
+
+        timer.start();
     }
-//generates board
-    public void createBoard()
+
+    public int movePlayer(int dx, int dy)
     {
-        traps = new Rectangle[totalTraps];
-        createTraps();
-        prizes = new Rectangle[totalPrizes];
-        createPrizes();
-        walls = new Rectangle[totalWalls];
-        createWalls();
-    }
-//made to move the player, and warn the players if they do not make smart moves... .. 
-    public int movePlayer(int incrx, int incry)
-    {
-        int newX = x + incrx;
-        int newY = y + incry;
+        int nx = x + dx;
+        int ny = y + dy;
 
-        playerSteps++;
+        for (Rectangle r : walls)
+            if (r.contains(nx, ny))
+                return wallVal;
 
-        if ((newX < 0 || newX > WIDTH-SPACE_SIZE) || (newY < 0 || newY > HEIGHT-SPACE_SIZE))
-        {
-            System.out.println("OFF THE GRID!");
-            return -offGridVal;
-        }
-
-        for (Rectangle r: walls)
-        {
-            int startX = (int) r.getX();
-            int endX = startX + (int) r.getWidth();
-            int startY = (int) r.getY();
-            int endY = startY + (int) r.getHeight();
-
-            if ((incrx > 0 && x <= startX && startX <= newX && y >= startY && y <= endY) ||
-                (incrx < 0 && x >= startX && startX >= newX && y >= startY && y <= endY) ||
-                (incry > 0 && y <= startY && startY <= newY && x >= startX && x <= endX) ||
-                (incry < 0 && y >= startY && startY >= newY && x >= startX && x <= endX))
-            {
-                System.out.println("A WALL IS IN THE WAY");
-                return -hitWallVal;
-            }
-        }
-//increments score
-        x += incrx;
-        y += incry;
+        x = nx;
+        y = ny;
+        steps++;
         repaint();
         return 0;
     }
-//a boolean to check if the given rectangle is a trap, provides result to the user
-    public boolean isTrap(int newx, int newy)
-    {
-        double px = playerLoc.getX() + newx;
-        double py = playerLoc.getY() + newy;
 
-        for (Rectangle r: traps)
-        {
-            if (r.getWidth() > 0 && r.contains(px, py))
-            {
-                System.out.println("A TRAP IS AHEAD");
+    public boolean isTrap(int dx, int dy)
+    {
+        double px = x + dx;
+        double py = y + dy;
+
+        for (Rectangle r : traps)
+            if (r.getWidth()>0 && r.contains(px, py))
                 return true;
-            }
-        }
 
         return false;
     }
 
-    public int springTrap(int newx, int newy)
+    public int springTrap(int dx, int dy)
     {
-        double px = playerLoc.getX() + newx;
-        double py = playerLoc.getY() + newy;
-//if the player encounters a trap 
-        for (Rectangle r: traps)
+        double px = x + dx;
+        double py = y + dy;
+
+        for (Rectangle r : traps)
         {
-            if (r.contains(px, py) && r.getWidth() > 0)
+            if (r.getWidth()>0 && r.contains(px, py))
             {
                 r.setSize(0,0);
-                System.out.println("TRAP IS SPRUNG!");
                 return trapVal;
             }
         }
-//if the player uses the command without the trap 
-        System.out.println("THERE IS NO TRAP HERE TO SPRING");
         return -trapVal;
     }
 
     public int pickupPrize()
     {
-        double px = playerLoc.getX();
-        double py = playerLoc.getY();
-//if the player succesfully reaches a prize
-        for (Rectangle p: prizes)
+        for (Ellipse2D.Double e : prizes)
         {
-            if (p.getWidth() > 0 && p.contains(px, py))
+            if (e.width>0 && e.contains(x, y))
             {
-                System.out.println("YOU PICKED UP A PRIZE!");
-                p.setSize(0,0);
-                repaint();
-                return prizeVal;
+                e.width = 0;
+                e.height = 0;
+                return scoreVal;
             }
         }
-//if the player fails to reach a prize 
-        System.out.println("OOPS, NO PRIZE HERE");
-        return -prizeVal;
+        return 0;
     }
-//sets quantity limits on each portion, and sets steps back to the start
-    public int getSteps() { return playerSteps; }
 
-    public void setPrizes(int p) { totalPrizes = p; }
-
-    public void setTraps(int t) { totalTraps = t; }
-
-    public void setWalls(int w) { totalWalls = w; }
-//provides an option to replay the game 
-    public int replay()
-    {
-        int win = playerAtEnd();
-
-        for (Rectangle p: prizes) p.setSize(SPACE_SIZE/3, SPACE_SIZE/3);
-        for (Rectangle t: traps) t.setSize(SPACE_SIZE/3, SPACE_SIZE/3);
-
-        x = START_LOC_X;
-        y = START_LOC_Y;
-        playerSteps = 0;
-        repaint();
-        return win;
-    }
-//provides a pathway to call for the player quitting or winning 
     public int endGame()
     {
-        int win = playerAtEnd();
-        setVisible(false);
-        frame.dispose();
-        return win;
+        if (x >= goal)
+            return 100;
+        return 0;
     }
-//adds in background components 
+
+    public int getSteps()
+    {
+        return steps;
+    }
+
+    public int replay()
+    {
+        int oldSteps = steps;
+        createBoard();
+        return -oldSteps;
+    }
+
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
 
-        g.drawImage(bgImage, 0, 0, null);
+        for (Rectangle r : walls)
+            g2.drawImage(rock, r.x, r.y, r.width, r.height, null);
 
-        for (Rectangle t: traps)
-        {
-            g2.setPaint(Color.WHITE);
-            g2.fill(t);
-        }
-//draws out the prize
-        for (Rectangle p: prizes)
-        {
-            if (p.getWidth() > 0)
-            {
-                int px = (int)p.getX();
-                int py = (int)p.getY();
-                g.drawImage(prizeImage, px, py, null);
-            }
-        }
-//draws out walls
-        for (Rectangle r: walls)
-        {
-            if (rockImage != null)
-            {
-                g.drawImage(rockImage, (int)r.getX(), (int)r.getY(), r.width, r.height, null);
-            }
-            else
-            {
-                g2.setPaint(Color.BLACK);
-                g2.fill(r);
-            }
-        }
-//sets location and adds the Duke, how villanous 
-        if (dukeImage != null)
-        {
-            g.drawImage(dukeImage, dukeX, dukeY, dukeWidth, dukeHeight, null);
-        }
+        for (Rectangle t : traps)
+            if (t.width>0)
+                g2.setColor(Color.WHITE);
 
-        g.drawImage(player, x, y, 40, 40, null);
-        playerLoc.setLocation(x, y);
+        g2.setColor(Color.WHITE);
+        for (Rectangle t : traps)
+            if (t.width>0)
+                g2.fill(t);
+
+        g2.drawImage(player, x-20, y-20, 40, 40, null);
+        g2.drawImage(flag, goal-40, s/2-20, 40, 40, null);
+
+        g2.setColor(Color.YELLOW);
+        for (Ellipse2D.Double e : prizes)
+            if (e.width>0)
+                g2.fill(e);
     }
-//draws random prizes on the map, but limits their size
-    private void createPrizes()
+
+    public void actionPerformed(ActionEvent e)
     {
-        int s = SPACE_SIZE;
-        Random rand = new Random();
-        for (int i = 0; i < totalPrizes; i++)
-        {
-            int h = rand.nextInt(GRID_H);
-            int w = rand.nextInt(GRID_W);
-            Rectangle r = new Rectangle((w*s + 15), (h*s + 15), 15, 15);
-            prizes[i] = r;
-        }
-    }
-//draws random traps in the map, but limits their size
-    private void createTraps()
-    {
-        int s = SPACE_SIZE;
-        Random rand = new Random();
-        for (int i = 0; i < totalTraps; i++)
-        {
-            int h = rand.nextInt(GRID_H);
-            int w = rand.nextInt(GRID_W);
-            Rectangle r = new Rectangle((w*s + 15), (h*s + 15), 15, 15);
-            traps[i] = r;
-        }
-    }
-//draws random walls in the map 
-    private void createWalls()
-    {
-        int s = SPACE_SIZE;
-        Random rand = new Random();
-        for (int i = 0; i < totalWalls; i++)
-        {
-            int h = rand.nextInt(GRID_H);
-            int w = rand.nextInt(GRID_W);
-            Rectangle r;
-            if (rand.nextInt(2) == 0)
-            {
-                r = new Rectangle((w*s + s - 5), h*s, 8, s);
-            }
-            else
-            {
-                r = new Rectangle(w*s, (h*s + s - 5), s, 8);
-            }
-            walls[i] = r;
-        }
-    }
-//provides result depending on player quitting or maintaining their progress to the end 
-    private int playerAtEnd()
-    {
-        double px = playerLoc.getX();
-        if (px > (WIDTH - 2*SPACE_SIZE))
-        {
-            System.out.println("YOU MADE IT!");
-            return endVal;
-        }
-        else
-        {
-            System.out.println("OOPS, YOU QUIT TOO SOON!");
-            return -endVal;
-        }
+        repaint();
     }
 }
